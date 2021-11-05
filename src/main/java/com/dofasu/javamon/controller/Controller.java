3d0f63e42321d921e1d9a15087104490a7a6fcd3
Fan <fanwu513@gmail.com>
2021-11-05 13:30:59 -0400

    Updated Controller

--- src/main/java/com/dofasu/javamon/controller/Controller.java
+++ src/main/java/com/dofasu/javamon/controller/Controller.java
@@ -1,7 +1,80 @@
 package com.dofasu.javamon.controller;
 
+import com.dofasu.javamon.actions.Attack;
+import com.dofasu.javamon.models.Javamon;
+import com.dofasu.javamon.models.AttackType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Controller {
     public static void startGame() {
         System.out.println("Started game");
+    }
+
+    public Collection<Javamon> buildJavamon(){
+
+        Collection<Javamon> javamonList = new ArrayList<>();
+
+        List<Attack> pikachuAttacks = Arrays.asList(
+                new Attack("Thunderbolt", AttackType.ELECTRIC, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Scratch", AttackType.NORMAL,50,80),
+                new Attack("Sand Attack", AttackType.GROUND, 60, 80));
+        Javamon pikachu = new Javamon("Pikachu", pikachuAttacks, 100, "url");
+
+        List<Attack> charmanderAttacks = Arrays.asList(
+                new Attack("Flamethrower", AttackType.FIRE, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Scratch", AttackType.NORMAL,50,80),
+                new Attack("Sand Attack", AttackType.GROUND, 60, 80));
+        Javamon charmander = new Javamon("Charmander", charmanderAttacks, 100, "url");
+
+        List<Attack> squirtleAttacks = Arrays.asList(
+                new Attack("Water gun", AttackType.WATER, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Scratch", AttackType.NORMAL,50,80),
+                new Attack("Sand Attack", AttackType.GROUND, 60, 80));
+        Javamon squirtle = new Javamon("Squirtle", squirtleAttacks, 100, "url");
+
+        List<Attack> bulbasaurAttacks = Arrays.asList(
+                new Attack("Leaf Blade", AttackType.WATER, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Bite", AttackType.NORMAL,50,80),
+                new Attack("Sand Attack", AttackType.GROUND, 60, 80));
+        Javamon bulbasaur = new Javamon("Bulbasaur", bulbasaurAttacks, 100, "url");
+
+        List<Attack> geodudeAttacks = Arrays.asList(
+                new Attack("Rock Throw", AttackType.ROCK, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Punch", AttackType.NORMAL,50,80),
+                new Attack("Sand Attack", AttackType.GROUND, 60, 80));
+        Javamon geodude = new Javamon("Geodude", geodudeAttacks, 100, "url");
+
+        List<Attack> pidgeottoAttacks = Arrays.asList(
+                new Attack("Air Slash", AttackType.FLYING, 80, 80),
+                new Attack("Tackle", AttackType.NORMAL, 60, 80),
+                new Attack("Scratch", AttackType.NORMAL,50,80),
+                new Attack("Gust", AttackType.FLYING, 60, 80));
+        Javamon pidgeotto = new Javamon("Pidgeotto", pidgeottoAttacks, 100, "url");
+
+        List<Attack> snorlaxAttacks = Arrays.asList(
+                new Attack("Earthquake", AttackType.GROUND, 80, 80),
+                new Attack("Body Slam", AttackType.NORMAL, 60, 80),
+                new Attack("Punch", AttackType.NORMAL,50,80),
+                new Attack("Roll", AttackType.NORMAL, 60, 80));
+        Javamon snorlax = new Javamon("Snorlax", snorlaxAttacks, 100, "url");
+
+        javamonList.add(pikachu);
+        javamonList.add(charmander);
+        javamonList.add(squirtle);
+        javamonList.add(bulbasaur);
+        javamonList.add(geodude);
+        javamonList.add(pidgeotto);
+        javamonList.add(snorlax);
+
+        return javamonList;
     }
 }
